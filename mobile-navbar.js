36652b91efde6b879c8196053e154d28cd3a262f
jaqueline bastos <147009051+jaquebastos@users.noreply.github.com>
2024-12-06 19:31:02 -0300

    alterações Nic mobile-navbar.js

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,7 +1,8 @@
-const menuButton = document.querySelector('.menu-mobile');
-const mobileMenu = document.querySelector('.navbar-mobile');
+// Seleciona os elementos
+const menuMobile = document.querySelector('.menu-mobile');
+const navbarMobile = document.querySelector('.navbar-mobile');
 
-menuButton.addEventListener('click', () => {
-    menuButton.classList.toggle('open');
-    mobileMenu.classList.toggle('open');
+// Adiciona o evento de clique para abrir/fechar o menu
+menuMobile.addEventListener('click', () => {
+  navbarMobile.classList.toggle('active');
 });
